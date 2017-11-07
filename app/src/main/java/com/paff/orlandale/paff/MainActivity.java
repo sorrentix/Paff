ba709bf740e6210389dba8870ef22184f1d5d8d3
ooorlandooo <o.aprea@gmail.com>
2017-11-07 17:01:07 +0100

    Paff: add badlogic framework

--- app/src/main/java/com/paff/orlandale/paff/MainActivity.java
+++ app/src/main/java/com/paff/orlandale/paff/MainActivity.java
@@ -2,6 +2,7 @@ package com.paff.orlandale.paff;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.badlogic.androidgames.framework.*;
 
 public class MainActivity extends AppCompatActivity {
 
